ceff81c09bfb80dd4cc106306d6ea511b07c4179
Bhupendra Godara <bhupen1005@gmail.com>
2025-03-19 20:14:08 +0530

    x-clone-ui

---
+++ x-clone-ui-completed/src/actions.tsx
@@ -0,0 +1,45 @@
+"use server";
+
+import { imagekit } from "./utils";
+
+export const shareAction = async (
+  formData: FormData,
+  settings: { type: "original" | "wide" | "square"; sensitive: boolean }
+) => {
+  // In below line of code, we are getting the file from the form data.
+  const file = formData.get("file") as File;
+  // const desc = formData.get("desc") as string;
+
+
+  // In below code, we are converting the file to a buffer and then uploading it to ImageKit.
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const transformation = `w-600, ${
+    settings.type === "square"
+      ? "ar-1-1"
+      : settings.type === "wide"
+      ? "ar-16-9"
+      : ""
+  }`;
+
+  imagekit.upload(
+    {
+      file: buffer,
+      fileName: file.name,
+      folder: "/posts",
+      ...(file.type.includes("image") && {
+        transformation: {
+          pre: transformation,
+        },
+      }),
+      customMetadata: {
+        sensitive: settings.sensitive,
+      },
+    },
+    function (error, result) {
+      if (error) console.log(error);
+      else console.log(result);
+    }
+  );
+};
